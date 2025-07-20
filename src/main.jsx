a8ca014c2ffec9f4da16216aa4af091865e08757
Sandeep6268 <sandeepsinghkhicchi@example.com>
2025-07-21 01:12:47 +0530

    cursor updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import SmokeyCursor from './SmokeyCursor.js'
 
 createRoot(document.getElementById('root')).render(
   <>
+  <SmokeyCursor/>
     <App />
   </>,
 )
---
+++ webpack.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  resolve: {
+    extensions: ['.tsx', '.ts', '.jsx', '.js']  // सभी एक्सटेंशन्स को सपोर्ट
+  }
+};
