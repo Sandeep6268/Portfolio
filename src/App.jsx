08d83b24a53719b600e5db2f1d07c2e8e0dfc521
Sandeep6268 <sandeepsinghkhicchi@example.com>
2025-07-21 08:56:44 +0530

    updated cursor

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import { motion, AnimatePresence } from "framer-motion";
-import * as THREE from 'three'
-window.THREE = THREE
+import * as THREE from 'three';
+window.THREE = THREE;
 import {
   FiGithub,
   FiExternalLink,
--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['three'], // three.js को एक्सटर्नल डिपेंडेंसी के रूप में मार्क करें
-    },
-  },
-  optimizeDeps: {
-    include: ['three'], // three.js को ऑप्टिमाइज़ेशन में शामिल करें
+  resolve: {
+    alias: {
+      three: 'three/build/three.module.js'
+    }
   }
 })
